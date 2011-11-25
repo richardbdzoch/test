1cbe144b639557cc10f0af3fa91482dd25449f5e
Richard Bdzoch <richard.bdzoch@gmail.com>
2011-11-25 14:45:33 +0100

    added win

---
+++ win.php
@@ -0,0 +1,29 @@
+<?php
+
+$word = $_GET['word'];
+
+$pSegment = $page->addChild("pageSegment");
+$pSegment->addAttribute("x", "10");
+$pSegment->addAttribute("y", "30");
+$pSegment->addAttribute("w", "width - 10");
+
+$fixed = $pSegment->addChild("fixed");
+$text = $fixed->addChild("text", "Word: " . strtoupper($word));
+$text->addAttribute("x", "0");
+$text->addAttribute("w", "width");
+$text->addAttribute("y", "0");
+$text->addAttribute("mode", "wrap");
+$text->addAttribute("style", "list_text");
+$text->addAttribute("align", "left");
+
+$text = $fixed->addChild("text", "You win!");
+$text->addAttribute("x", "0");
+$text->addAttribute("w", "width");
+$text->addAttribute("y", "40");
+$text->addAttribute("mode", "wrap");
+$text->addAttribute("style", "list_text");
+$text->addAttribute("align", "left");
+
+addHeader($bml, $app_settings);
+addFooterMenu($bml, $app_settings); 
+?>
