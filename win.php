b97de9db6b13e8f9563b55d0a39a0f9b1db8ad5a
Richard Bdzoch <richard.bdzoch@gmail.com>
2011-11-25 14:48:56 +0100

    TEST-2 TEST-1 cmmt

--- win.php
+++ win.php
@@ -5,16 +5,16 @@ $word = $_GET['word'];
 $pSegment = $page->addChild("pageSegment");
 $pSegment->addAttribute("x", "10");
 $pSegment->addAttribute("y", "30");
-$pSegment->addAttribute("w", "width - 10");
+edited
 
+
+added
 $fixed = $pSegment->addChild("fixed");
 $text = $fixed->addChild("text", "Word: " . strtoupper($word));
 $text->addAttribute("x", "0");
 $text->addAttribute("w", "width");
 $text->addAttribute("y", "0");
-$text->addAttribute("mode", "wrap");
-$text->addAttribute("style", "list_text");
-$text->addAttribute("align", "left");
+3 removed
 
 $text = $fixed->addChild("text", "You win!");
 $text->addAttribute("x", "0");
